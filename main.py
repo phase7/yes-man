128e4e7386616736808143dc8b04261d97409ac1
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2023-11-25 09:29:01 +0000

    Ai generated bug fix

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Callable
 from starlette.responses import JSONResponse
 import logging
-
-
-def log_requests(app: FastAPI) -> Callable:
-    logger = logging.getLogger("requests")
-
-    @app.middleware("http")
-    async def log_requests_middleware(
-        request: Request, call_next: Callable
-    ) -> JSONResponse:
-        logger.info(f"{request.method} {request.url}")
-        response = await call_next(request)
-        return JSONResponse(content=response.body, status_code=200)
-
-    return logger.info
-
-
-def handle_exceptions(app: FastAPI) -> Callable:
-    @app.exception_handler(Exception)
-    async def handle_exceptions_middleware(
-        request: Request, exc: Exception
-    ) -> JSONResponse:
-        return JSONResponse(content="OK", status_code=200)
-
-    return print
-
 
 app: FastAPI = FastAPI()
 
@@ -37,6 +12,31 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def log_requests(app: FastAPI) -> Callable:
+    logger = logging.getLogger("requests")
+
+    @app.middleware("http")
+    async def log_requests_middleware(
+        request: Request, call_next: Callable
+    ) -> Response:
+        print(request.url)
+        response : Response = await call_next(request)
+        return response
+
+    return logger.info
+
+
+def handle_exceptions(app: FastAPI) -> Callable:
+    @app.exception_handler(Exception)
+    async def handle_exceptions_middleware(
+        request: Request, exc: Exception
+    ) -> JSONResponse:
+        return JSONResponse(content="exception occurred in code", status_code=200)
+
+    return print
+
+
 
 logger: Callable = log_requests(app)
 exception_handler: Callable = handle_exceptions(app)
