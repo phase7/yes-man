46d47c18dbc18c9ef53551a30a2305693928309d
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2023-11-25 09:48:56 +0000

    redundant code fix

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Callable
+from typing import Callable, NoReturn
 from starlette.responses import JSONResponse
-import logging
 
 app: FastAPI = FastAPI()
 
@@ -13,33 +12,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def log_requests(app: FastAPI) -> Callable:
-    logger = logging.getLogger("requests")
 
+def log_requests(app: FastAPI) -> NoReturn:
     @app.middleware("http")
     async def log_requests_middleware(
         request: Request, call_next: Callable
     ) -> Response:
         print(request.url)
-        response : Response = await call_next(request)
+        response: Response = await call_next(request)
         return response
 
-    return logger.info
 
-
-def handle_exceptions(app: FastAPI) -> Callable:
+def handle_exceptions(app: FastAPI) -> NoReturn:
     @app.exception_handler(Exception)
     async def handle_exceptions_middleware(
         request: Request, exc: Exception
     ) -> JSONResponse:
         return JSONResponse(content="exception occurred in code", status_code=200)
 
-    return print
 
-
-
-logger: Callable = log_requests(app)
-exception_handler: Callable = handle_exceptions(app)
+log_requests(app)
+handle_exceptions(app)
 
 
 @app.get("/{path}")
