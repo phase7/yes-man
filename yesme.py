d9194fbf15c339c2a19753f7f107b63f02628c1c
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2023-11-26 15:32:57 +0000

    added loggin as csv file

--- yesme.py
+++ yesme.py
@@ -1,14 +1,23 @@
+import logging
 from fastapi import FastAPI, Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
+
+handler = logging.FileHandler("yes.log.csv", "a", "utf-8")
+formatter = logging.Formatter("%(asctime)s,%(thread)d,%(levelname)s,%(message)s")
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("yes-man")
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 
 class PrintGetRequestDetailsMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        print(f"{request.method=} {request.url.path=}")
+        if "favicon.ico" in request.url.path:
+            return Response(content="favicon")
+        logger.debug(f"{request.method=} {request.url.path=}")
         return Response(content="OK")
 
 
 app = FastAPI()
 app.add_middleware(PrintGetRequestDetailsMiddleware)
-
-
